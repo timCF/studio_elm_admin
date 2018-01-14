288fb15450c013048db6f51493c08fd3c2061149
Tkachuk Ilya <tim@Tkachuks-iMac.local>
2018-01-15 00:25:54 +0200

    basic app skeleton

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,50 @@
-import Html
+module Main exposing (..)
+import Html exposing (..)
+
+--port toElm : (String -> msg) -> Sub msg
+
+-- MAIN
+
+main : Program Never Model Msg
+main =
+    Html.program
+        {
+          init          = init,
+          update        = update,
+          view          = view,
+          subscriptions = subscriptions
+        }
 
 -- MODEL
 
-type alias State =
-    {
-      login : String,
-      password : String
-    }
+type alias Model =
+  {
+    login : String,
+    password : String
+  }
 
+init : ( Model, Cmd Msg )
+init = ( {login = "Hello", password = "World"}, Cmd.none )
 
-initialState : State
-initialState =
-    {
-      login = "",
-      password = ""
-    }
+-- UPDATE
 
-main = Html.text "Hello World"
+type Msg
+  = DoNothing
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    DoNothing ->
+      ( model, Cmd.none )
+
+-- VIEW
+
+view : Model -> Html Msg
+view {login, password} =
+    div [] [text (login ++ " " ++ password)]
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
